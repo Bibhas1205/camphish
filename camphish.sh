9d91723a36a2728c4ca0075766bbfafb25ae7c46
Bibhas Das <125908181+Bibhas1205@users.noreply.github.com>
2023-06-28 13:51:01 +0000

    camphish.sh

--- camphish.sh
+++ camphish.sh
@@ -1,7 +1,5 @@
 sudo apt-get install curl
 sudo apt-get insatll git
-sudo git clone https://github.com/Bibhas1205/camphish.git
-sudo mv camphish/camphish.sh camphish.sh
 sudo mv camphish /var/www/html/
 sudo chmod 777 /var/www/html/*
 sudo service apache2 start
